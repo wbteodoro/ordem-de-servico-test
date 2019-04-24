01e02daed91a4245a81b1b0674aa1dbe3f62a6b9
Matheus Schlosser <matheuschlosser@gmail>
2019-04-24 16:36:49 -0300

    Criação card cliente TESTE

---
+++ src/modules/order-services/components/CardClient/CardCliente.test.jsx
@@ -0,0 +1,27 @@
+// import React from 'react'
+// import { shallow, mount } from 'enzyme'
+// import ListServices from './ListServices'
+// import sinon from 'sinon'
+
+// describe('Testing ListServices Component', () => {
+ 
+//   it('should show a message that any service was created', () => {
+//     const wrapper = shallow(
+//       <ListServices services={[]} 
+//         onClickServiceDeleteButton={()=> null}
+//         onClickServiceEditButton={()=> null}
+//       /> 
+//     ).dive()
+//     expect(wrapper.find('.no-services').length).toEqual(1);  
+//   })
+
+
+
+//   /**
+//   |--------------------------------------------------
+//   |     Tests using Mount to test events
+//   |--------------------------------------------------
+//   */
+
+// })
+
