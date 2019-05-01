093676aef637228d55dce4e52f6e388e50f4bd39
Matheus Schlosser <matheuschlosser@gmail>
2019-05-01 12:58:22 -0300

    Criação CardClient

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
+import CardClient from './modules/order-services/components/CardClient/CardClient';
 
 class App extends Component {
     render() {
         return (
             <div>
-                IT'S WORKS!
+                IT WORKS!
             </div>
-        );
+        )
     }
 }
 
--- src/modules/order-services/components/CardClient/CardClient.jsx
+++ src/modules/order-services/components/CardClient/CardClient.jsx
@@ -1,63 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
-
-const styles = {
-    card: {
-      minWidth: 275,
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-  };
-
-
-function SimpleCard(props) {
-    
-    const bull = <span className={classes.bullet}>•</span>; 
 
 
 class CardClient extends Component {
   render() {
+
     return (
-        <Card className={classes.card}>
+      <Card>
         <CardContent>
-            <Typography className={classes.title} color="textSecondary" gutterBottom>
-            Cliente
+          {/* As informações virão da variavel dados do FormClient*/}
+            <Typography color="textSecondary" gutterBottom>
+              Nome: {this.props.serviceOrder.name}
             </Typography>
-            <Typography className={classes.pos} color="textSecondary">
-            adjective
+            <Typography color="textSecondary" gutterBottom>
+              CPF: {this.props.serviceOrder.cpf}
             </Typography>
-            <Typography component="p">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
+            <Typography color="textSecondary" gutterBottom>
+              CNPJ: {this.props.serviceOrder.cnpf}
+            </Typography>
+            <Typography color="textSecondary" gutterBottom>
+              Telefone: {this.props.serviceOrder.phone}
+            </Typography>
+            <Typography color="textSecondary" gutterBottom>
+              Data de Nascimento: {this.props.serviceOrder.birth_date}
+            </Typography>
+            <Typography color="textSecondary" gutterBottom>
+              E-mail: {this.props.serviceOrder.email}
+            </Typography>
+            <Typography color="textSecondary" gutterBottom>
+              Nome Fantasia:{this.props.serviceOrder.fantasy_name}
             </Typography>
         </CardContent>
-        <CardActions>
-            <Button size="small">Learn More</Button>
-        </CardActions>
-        </Card>
+      </Card>      
     )
-}
-}
-
-CardClient.PropTypes = {
-    classes: PropTypes.object.isRequired
+  }
 }
 
-export default withStyles(styles)(CardClient)
+
+CardClient.propTypes = {
+    serviceOrder: PropTypes.object.isRequired,
+}
+
+export default CardClient
